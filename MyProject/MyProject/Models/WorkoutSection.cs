8f2c64af555588ded939f41d9e5b7e035ba51522
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-04-28 20:18:17 +0200

    Workout section model added

---
+++ MyProject/MyProject/Models/WorkoutSection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class WorkoutSection
+    {
+        public int WorkoutSectionId { get; set; }
+        public string WorkoutSectionName { get; set; }
+    }
+}
