891752dc1410feffbe528851273c175d509a9d88
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-04-28 20:22:07 +0200

    Exercise model added

---
+++ MyProject/MyProject/Models/Exercises.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class Exercises
+    {
+        public int ExerciseId { get; set; }
+        public string ExerciseName { get; set; }
+        public string ExerciseReps { get; set; }
+        public int BreakTime { get; set; }
+    }
+}
