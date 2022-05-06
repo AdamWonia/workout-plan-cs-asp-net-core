61d9e0e493107de074d31baf41b94d2ed424f6d5
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-06 21:58:05 +0200

    big changes in model WorkoutSection

--- MyProject/MyProject/Models/WorkoutSection.cs
+++ MyProject/MyProject/Models/WorkoutSection.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyProject.Models
 {
     public class WorkoutSection
     {
+        // First part of the model - section in the Index
+        [Key]
         public int WorkoutSectionId { get; set; }
         public string WorkoutSectionName { get; set; }
 
+        // Second part of the model - exercises in a specific section
         public string ExerciseName { get; set; }
         public string ExerciseReps { get; set; }
         public int BreakTime { get; set; }
 
+        // Implementation of List properties - test
+        private List<string> _content = new List<string>();
+
+        [NotMapped]
+        public IEnumerable<string> Content
+        {
+            set { _content.Add(value.ToString()); }
+            // it is a list - this is why there is strange field in the form
+            get { return _content; }
+        }
+
+        public void AddContent(String argValue)
+        {
+            _content.Add(argValue);
+        }
+
         public WorkoutSection()
         {
-
-        }       
+            this.AddContent("test_X");
+        }
     }
 }
