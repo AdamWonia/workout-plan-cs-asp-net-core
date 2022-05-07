dca8bfd23043c25dc983e0151b6dcb3ed92ad18d
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-07 19:17:33 +0200

    changes in Workout model

--- MyProject/MyProject/Models/WorkoutSection.cs
+++ MyProject/MyProject/Models/WorkoutSection.cs
@@ -15,29 +15,34 @@ namespace MyProject.Models
         public string WorkoutSectionName { get; set; }
 
         // Second part of the model - exercises in a specific section
+        [Display(Name = "List of exercises")]
         public string ExerciseName { get; set; }
         public string ExerciseReps { get; set; }
         public int BreakTime { get; set; }
+
+
 
         // Implementation of List properties - test
         private List<string> _content = new List<string>();
 
         [NotMapped]
+        [Display(Name = "Add Exercise into the Section")]
         public IEnumerable<string> Content
         {
-            set { _content.Add(value.ToString()); }
             // it is a list - this is why there is strange field in the form
             get { return _content; }
         }
 
-        public void AddContent(String argValue)
+        public void AddContent(string Content_input)
         {
-            _content.Add(argValue);
+            _content.Add(Content_input);
         }
 
         public WorkoutSection()
         {
             this.AddContent("test_X");
+            this.AddContent("test_Y");
+            this.AddContent("test_Z");
         }
     }
 }
