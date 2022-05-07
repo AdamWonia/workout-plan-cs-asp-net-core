148ab18b8719329c866f4b1babd0c6846b7ba601
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-07 19:16:55 +0200

    added new migration

---
+++ MyProject/MyProject/Data/Migrations/20220507162102_Test2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Data.Migrations
+{
+    public partial class Test2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExerciseId",
+                table: "WorkoutSection");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExerciseId",
+                table: "WorkoutSection",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
