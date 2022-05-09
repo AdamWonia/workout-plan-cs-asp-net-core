95e4d2177e8b9607b88a74fc8855059a1ccc310f
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-09 19:51:39 +0200

    new model added for testing

---
+++ MyProject/MyProject/Models/Workout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    public class Workout
+    {
+        [Key]
+        public int WorkoutId { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Set> Sets { get; set; }
+    }
+
+    public class Set
+    {
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public int WorkoutId { get; set; }
+        public decimal Weight { get; set; }
+        public int Reps { get; set; }
+
+       // public virtual Exercise Exercise { get; set; }
+       // public virtual Workout Workout { get; set; }
+    }
+}
