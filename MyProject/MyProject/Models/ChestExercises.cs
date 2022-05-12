f8ef7a76ad653a9537020893f4d4303282e530fc
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-12 19:17:58 +0200

    new model added - chest

---
+++ MyProject/MyProject/Models/ChestExercises.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyProject.Models
+{
+    public class ChestExercises
+    {
+        [Key]
+        public int ExerciseId { get; set; }
+        public string ExerciseName { get; set; }
+        public int Reps { get; set; }
+        public int BreakTime { get; set; }
+    }
+}
