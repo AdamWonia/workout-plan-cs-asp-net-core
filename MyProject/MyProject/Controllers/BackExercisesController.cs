7bf3206b96aa5af087d0e5f07acbc4ff386be1a0
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-24 19:20:39 +0200

    Authorization safety added for create, edit and delete

--- MyProject/MyProject/Controllers/BackExercisesController.cs
+++ MyProject/MyProject/Controllers/BackExercisesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,7 @@ namespace MyProject.Controllers
         }
 
         // GET: BackExercises/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -52,6 +54,7 @@ namespace MyProject.Controllers
         // POST: BackExercises/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ExerciseId,BackPart,ExerciseName,Reps,BreakTime")] BackExercises backExercises)
@@ -66,6 +69,7 @@ namespace MyProject.Controllers
         }
 
         // GET: BackExercises/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -84,6 +88,7 @@ namespace MyProject.Controllers
         // POST: BackExercises/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ExerciseId,BackPart,ExerciseName,Reps,BreakTime")] BackExercises backExercises)
@@ -117,6 +122,7 @@ namespace MyProject.Controllers
         }
 
         // GET: BackExercises/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,6 +141,7 @@ namespace MyProject.Controllers
         }
 
         // POST: BackExercises/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
