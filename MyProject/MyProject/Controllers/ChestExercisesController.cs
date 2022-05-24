524b33534e04e730766962defbd89a1e082c7e7a
Adam Wonia <73058208+AdamWonia@users.noreply.github.com>
2022-05-24 20:06:10 +0200

    Authorization safety added for create, edit and delete

--- MyProject/MyProject/Controllers/ChestExercisesController.cs
+++ MyProject/MyProject/Controllers/ChestExercisesController.cs
@@ -27,7 +27,6 @@ namespace MyProject.Controllers
         }
 
         // GET: ChestExercises/Details/5
-        [Authorize]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
